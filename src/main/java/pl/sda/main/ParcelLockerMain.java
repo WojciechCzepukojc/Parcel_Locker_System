ce0b0b7db4cd4522ffad9dc1b96fae6fff6e871a
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-05-18 22:32:41 +0200

    "#2"

--- src/main/java/pl/sda/main/ParcelLockerMain.java
+++ src/main/java/pl/sda/main/ParcelLockerMain.java
@@ -18,6 +18,7 @@ public class ParcelLockerMain {
                 System.out.println("Wybierz co chcesz zrobić: ");
                 System.out.println("0. - Wyjście");
                 System.out.println("1. - Dodanie paczkomatu");
+                System.out.println("2. - Usunięcie paczkomatu");
 
                 menu = scanner.nextInt();
 
@@ -27,6 +28,8 @@ public class ParcelLockerMain {
                         System.exit(0);
                         break;
                     case 1: parcelLockerAdding();
+                        break;
+                    case 2: removeParcelLockerFromList();
                         break;
 
 
@@ -52,8 +55,19 @@ public class ParcelLockerMain {
             String id = parcelLockerServices.addNewParcelLocker(street, city, zipCode);
             System.out.println("Pomyślnie dodano paczkomat o ID: " + id );
 
-
         }
+
+    //#2
+    public static void removeParcelLockerFromList(){
+        System.out.println("Podaj ID paczkomatu który chcesz usunąć");
+            String id = scanner.next();
+            boolean result = parcelLockerServices.removeParcelLocker(id);
+            if (result){
+                System.out.println("Paczkomat o ID: " + id + " został poprawnie usunięty");
+            }else {
+                System.out.println("Nie udało się usunąć oaczkomatu o ID: " + id);
+            }
+    }
 
 
 
--- src/main/java/pl/sda/services/ParcelLockerServices.java
+++ src/main/java/pl/sda/services/ParcelLockerServices.java
@@ -9,14 +9,41 @@ import java.util.List;
 
 public class ParcelLockerServices {
 
-   private List<ParcelLocker> parcelLockers = new ArrayList<>();
+    private List<ParcelLocker> parcelLockers = new ArrayList<>();
 
-   //#1
-public String addNewParcelLocker(String street, String city, String zipCode){
-   Adress adress = new Adress(street, city, zipCode);
-   ParcelLocker parcelLocker = new ParcelLocker(adress);
-   parcelLockers.add(parcelLocker);
-   return parcelLocker.getID();
+    //#1
+    public String addNewParcelLocker(String street, String city, String zipCode) {
+        Adress adress = new Adress(street, city, zipCode);
+        ParcelLocker parcelLocker = new ParcelLocker(adress);
+        parcelLockers.add(parcelLocker);
+        return parcelLocker.getID();
+    }
+
+
+    //#2
+    public boolean removeParcelLocker(String id) {
+        ParcelLocker toRemove = null;
+        for (ParcelLocker parcelLocker : parcelLockers) {
+            if (parcelLocker.getID().equals(id)) {
+                toRemove = parcelLocker;
+                break;
+            }
+        }
+        if ((toRemove != null)) {
+            parcelLockers.remove(toRemove);
+            return true;
+        }
+        return false;
+
+    }
+
+
+    //#
+    private void printParcelLocker(List<ParcelLocker> parcelLockers) {
+        for (int i = 0; i < parcelLockers.size(); i++) {
+            System.out.println((i + 1) + ".- " + parcelLockers.get(i));
+        }
+    }
 }
 
 
@@ -25,4 +52,5 @@ public String addNewParcelLocker(String street, String city, String zipCode){
 
 
 
-}
+
+
